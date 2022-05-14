7b85fe45f45fe6cb51d8baae2fc25a2993d5001d
Ismail Bouhiri <ibouhiri@e1r3p6.1337.ma>
2022-05-14 22:19:55 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PhaserLearning
+# SRC of the game:
+#### http://phaser.io/tutorials/making-your-first-phaser-3-game/part6
---
+++ app.ts
@@ -0,0 +1,31 @@
+import Phaser from "phaser";
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game: Phaser.Game = new Phaser.Game(config);
+
+function preload () : void
+{
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 } );
+}
+
+function create () : void
+{
+}
+
+function update () : void
+{
+}
